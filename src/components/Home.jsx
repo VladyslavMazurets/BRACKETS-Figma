aeddf2be09b9c033a7e95d52154cad20aed3ca23
VladyslavMazurets <vladmazurec@gmail.com>
2023-05-11 16:01:19 +0200

    complete Navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
-import Navbar from './Navbar';
+import group from '../assets/img/group.png';
 
 function Home() {
   return (
-    <div className="relative bg-gradient-to-b from-indigo-900 to-purple-700">
-      <Navbar />
+    <div className="-mt-[100px]">
+      <div
+        className="h-screen bg-gradient-to-b 
+        from-indigo-800 to-purple-600"
+      >
+        <div
+          className="w-[689px] h-[425px] flex flex-col items-center 
+         mx-auto gap-[44px] pt-[159px]"
+        >
+          <p
+            className="h-[176px] w-[628px] flex flex-col items-center mt-[8px]
+              text-center font-poppins font-bold text-[70px] text-white"
+          >
+            <span className="flex">
+              New generation
+              <img
+                src={group}
+                alt="Group_img"
+                className="absolute left-[1265px] w-[32px] h-[40px] 
+                  object-contain"
+              />
+            </span>
+            of administration
+          </p>
+          <p>
+            Create beautiful admin panel, CRM or CMS for your laravel
+            application.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 
+import logo from '../assets/img/logo.png';
+import arrow from '../assets/img/arrow.png';
+
+const linkStyle = `w-[96px] h-[24px] flex items-center gap-[12px] font-poppins 
+text-white text-base cursor-pointer`;
+const arrowStyle = 'w-[12px] h-[16px]';
+const buttonStyle = 'font-poppins text-base text-white';
+
 function Navbar() {
   return (
     <nav
-      className="absolute top-0 left-[360px] w-[1200px] flex items-center 
-    py-[24px] gap-[88px]"
+      className="sticky top-0 mx-auto max-w-[1200px] h-[100px] flex 
+      items-center py-[24px] gap-[88px] bg-transparent border-0 border-solid"
     >
-      <div className="flex">
-        <div>logo</div>
-        <div>81</div>
-        <div>83</div>
+      <a href="/">
+        <img src={logo} alt="Logo" className="h-[35px] w-[194px]" />
+      </a>
+      <div className="w-[561px] max-h-[24px] flex items-start gap-[44px]">
+        <a className={linkStyle}>
+          <p>Features</p>
+          <img src={arrow} alt="Arrow" className={arrowStyle} />
+        </a>
+        <a className={`${linkStyle} w-[126px]`}>
+          <p>How it works</p>
+          <img src={arrow} alt="Arrow" className={arrowStyle} />
+        </a>
+        <a className={`${linkStyle} w-[81px]`}>
+          <p>Pricing</p>
+          <img src={arrow} alt="Arrow" className={arrowStyle} />
+        </a>
+      </div>
+      <div className="w-[269px] h-[52px] flex items-center gap-[22px]">
+        <button className={`${buttonStyle} w-[41px]`}>Docs</button>
+        <button className={`${buttonStyle} w-[49px]`}>Demo</button>
+        <button
+          className={`${buttonStyle} px-[24px] py-[14px] border-2 
+        rounded-[12px] hover:bg-white hover:text-black`}
+        >
+          <p className="w-[87px]">Try for free</p>
+        </button>
       </div>
     </nav>
   );
